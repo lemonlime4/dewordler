36b96a1478ede971065951306256016efdb8e4a0
lemonlime4 <71093251+User2573@users.noreply.github.com>
2025-12-15 18:14:48 -0500

    single word input

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,62 @@
+import { createEffect, createSignal, onCleanup } from 'solid-js';
 import './App.css';
-
-function App() {
-    // const [count, setCount] = createSignal(0);
-
-    return (
-        <>
-            <h1>What the fuck</h1>
-            <WordInput />
-        </>
-    );
-}
 
 export default App;
 
-function WordInput() {
-    return (
-        <div class="word-input">
-            <LetterInput />
-            <LetterInput />
-            <LetterInput />
-            <LetterInput />
-            <LetterInput />
-        </div>
+function App() {
+    const [letters, setLetters] = createSignal<string[]>([]);
+    const boxes: HTMLElement[] = [];
+    createEffect(() => {
+        // console.log(letters);
+    });
+    let listener;
+    window.addEventListener(
+        'keydown',
+        (listener = (ev: KeyboardEvent) => {
+            if (ev.key === 'Tab') {
+            } else if (ev.key === 'Delete' || (ev.key === 'Backspace' && ev.ctrlKey)) {
+                setLetters([]);
+            } else if (ev.key === 'Backspace') {
+                setLetters(letters().slice(0, -1));
+            } else {
+                const letter = readLetter(ev);
+                if (letter !== undefined) {
+                    if (letters().length < 5) {
+                        setLetters([...letters(), letter]);
+                    }
+                }
+            }
+            if (document.activeElement instanceof HTMLElement) {
+                document.activeElement.blur();
+            }
+            // boxes[letters().length]?.focus();
+        })
     );
-}
+    onCleanup(() => window.removeEventListener('keydown', listener));
 
-function LetterInput() {
-    // function handleInput(event: InputEvent) {
-    //     if (!event.target) throw new Error('Input event has no target');
-    //     if (!event.data) return;
-    //     let letters = [...event.data.normalize('NFKD')].filter((c) => {
-    //         let n = c.charCodeAt(0);
-    //         return (96 < n && n < 123) || (64 < n && n < 91);
-    //     });
-    //     event.target.value = letters.at(-1);
-    // }
-    function handleInput(event: KeyboardEvent) {
-        event.preventDefault();
-        console.log(event.key);
-
-        // navigation
-        if (event.key === 'ArrowLeft') {
-        }
-        if (event.key === 'ArrowRight') {
-        }
-        if (event.key === 'Tab') {
-        }
+    function readLetter(ev: KeyboardEvent): string | undefined {
+        if (ev.key.normalize('NFKC').length !== 1) return;
+        console.log([...ev.key.normalize('NFKD')]);
+        return [...ev.key.normalize('NFKD')]
+            .map(s => s.toUpperCase())
+            .find(s => {
+                const n = s.codePointAt(0) ?? 0;
+                return 65 <= n && n <= 90;
+            });
     }
-    return <input onkeydown={handleInput} class="letter-input" />;
+
+    return (
+        <>
+            <h1>What the</h1>
+            <div id="input">
+                <div class="word">
+                    {[0, 1, 2, 3, 4].map(i => (
+                        <div ref={el => (boxes[i] = el)} class="letter">
+                            <span>{letters()[i] ?? ''}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 }
